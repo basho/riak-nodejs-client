4f9bee0d26c1ad75587b0ed3faed575abe8856d5
Luke Bakken <luke@bakken.io>
2015-03-18 09:01:58 -0700

    Configure default Grunt task to run jshint and mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,18 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
-      /*
-      options: {
-        globals: {
-          jQuery: true
-        }
-      }
-      */
     },
+    mochaTest: {
+        test: {
+            options: {
+                reporter: 'spec',
+                captureFile: 'results.txt',
+                quiet: false,
+                clearRequireCache: false
+            },
+            src: ['test/**/*.js']
+        }
+    },   
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
@@ -18,8 +22,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
