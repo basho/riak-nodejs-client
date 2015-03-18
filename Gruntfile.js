0187d675254712d928225fc876284be136de3ab4
Luke Bakken <luke@bakken.io>
2015-03-18 13:25:20 -0700

    Add docs task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    yuidoc: {
+        compile: {
+            name: '<%= pkg.name %>',
+            description: '<%= pkg.description %>',
+            version: '<%= pkg.version %>',
+            url: '<%= pkg.homepage %>',
+            options: {
+                paths: './lib/',
+                outdir: './docs/'
+            }
+        }
+    },
     jshint: {
       files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
     },
@@ -24,7 +37,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
 
   grunt.registerTask('default', ['jshint', 'mochaTest']);
+  grunt.registerTask('docs', ['yuidoc']);
 
 };
