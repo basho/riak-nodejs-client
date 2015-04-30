9c8a97625cdb162d9bee4889e675eb6b73a91f39
Luke Bakken <lbakken@basho.com>
2015-04-30 06:55:07 -0700

    Ensure that RiakCluster uses a new instance of DefaultNodeManager per-instance
    Fixes issue #49

---
+++ test/unit/riakcluster.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2014 Basho Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var RiakCluster = require('../../lib/core/riakcluster');
+var assert = require('assert');
+
+describe('RiakCluster', function() {
+    describe('schema defaults', function() {
+        it('should create new node manager per-cluster', function(done) {
+            var cluster1 = new RiakCluster();
+            var cluster2 = new RiakCluster();
+            assert.notStrictEqual(cluster1.nodeManager, cluster2.nodeManager);
+            done();
+        });
+    });
+});
