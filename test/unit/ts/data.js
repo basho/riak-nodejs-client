738d3297466f31be94e75e84d424f674fa19e01b
Luke Bakken <luke@bakken.io>
2015-10-30 14:04:13 -0700

    TS Get unit tests in progress.

--- test/unit/ts/data.js
+++ test/unit/ts/data.js
@@ -3,9 +3,12 @@
 var rpb = require('../../../lib/protobuf/riakprotobuf');
 var TsColumnDescription = rpb.getProtoFor('TsColumnDescription');
 var TsQueryResp = rpb.getProtoFor('TsQueryResp');
+var TsGetResp = rpb.getProtoFor('TsGetResp');
+var TsColumnType = rpb.getProtoFor('TsColumnType');
 var TsRow = rpb.getProtoFor('TsRow');
 var TsCell = rpb.getProtoFor('TsCell');
 
+var assert = require('assert');
 var crypto = require('crypto');
 var logger = require('winston');
 var Long = require('long');
@@ -102,3 +105,67 @@ var tsQueryResp = new TsQueryResp();
 Array.prototype.push.apply(tsQueryResp.columns, rpbcols);
 Array.prototype.push.apply(tsQueryResp.rows, rpbrows);
 module.exports.tsQueryResp = tsQueryResp;
+
+var tsGetResp = new TsGetResp();
+Array.prototype.push.apply(tsGetResp.columns, rpbcols);
+Array.prototype.push.apply(tsGetResp.rows, rpbrows);
+module.exports.tsGetResp = tsGetResp;
+
+function validateResponse(actual, expected) {
+    assert(actual.columns, 'expected columns in response');
+    assert(actual.rows, 'expected rows in response');
+
+    var rc = actual.columns;
+    assert.strictEqual(rc.length, expected.columns.length);
+    assert.strictEqual(rc[0].name, 'col_binary');
+    assert.strictEqual(rc[0].type, TsColumnType.BINARY);
+    assert.strictEqual(rc[0].type, TS.ColumnType.Binary);
+    assert.strictEqual(rc[1].name, 'col_int64');
+    assert.strictEqual(rc[1].type, TsColumnType.SINT64);
+    assert.strictEqual(rc[1].type, TS.ColumnType.Int64);
+    assert.strictEqual(rc[2].name, 'col_double');
+    assert.strictEqual(rc[2].type, TsColumnType.DOUBLE);
+    assert.strictEqual(rc[2].type, TS.ColumnType.Double);
+    assert.strictEqual(rc[3].name, 'col_timestamp');
+    assert.strictEqual(rc[3].type, TsColumnType.TIMESTAMP);
+    assert.strictEqual(rc[3].type, TS.ColumnType.Timestamp);
+    assert.strictEqual(rc[4].name, 'col_boolean');
+    assert.strictEqual(rc[4].type, TsColumnType.BOOLEAN);
+    assert.strictEqual(rc[4].type, TS.ColumnType.Boolean);
+    assert.strictEqual(rc[5].name, 'col_ms');
+    assert.strictEqual(rc[5].type, TsColumnType.TIMESTAMP);
+    assert.strictEqual(rc[5].type, TS.ColumnType.Timestamp);
+
+    var rr = actual.rows;
+    assert.strictEqual(rr.length, expected.rows.length);
+
+    var r0 = rr[0];
+    assert(r0[0] instanceof Buffer);
+    assert(bd0.equals(r0[0]));
+    assert(r0[1].equals(Long.ZERO));
+    assert.strictEqual(r0[2], 1.2);
+    assert(r0[3] instanceof Long);
+    assert(ts0ms.equals(r0[3]));
+    assert.strictEqual(r0[4], true);
+    assert(ts0ms.equals(r0[5]));
+
+    var r1 = rr[1];
+    assert(r1[0] instanceof Buffer);
+    assert(bd1.equals(r1[0]));
+    assert(r1[1].equals(3));
+    assert.strictEqual(r1[2], 4.5);
+    assert(r1[3] instanceof Long);
+    assert(ts1ms.equals(r1[3]));
+    assert.strictEqual(r1[4], false);
+    assert(ts1ms.equals(r1[5]));
+
+    var r2 = rr[2];
+    assert.strictEqual(r2[0], null);
+    assert(r2[1].equals(6));
+    assert.strictEqual(r2[2], 7.8);
+    assert.strictEqual(r2[3], null);
+    assert.strictEqual(r2[4], false);
+    assert.strictEqual(r2[5], null);
+}
+
+module.exports.validateResponse = validateResponse;
--- test/unit/ts/get.js
+++ test/unit/ts/get.js
@@ -12,62 +12,59 @@ if (!assert.deepStrictEqual) {
 var logger = require('winston');
 var Long = require('long');
 
-var queryText = 'select * from foo where baz = "bat"';
+var table = 'test-table';
+var key = [ 'foo', 'bar', 'baz' ];
 
-describe('Query', function() {
+describe('Get', function() {
 
     this.timeout(250);
 
     describe('Build', function() {
-        it('should build a TsQueryReq correctly', function(done) {
-            var queryCommand = new TS.Query.Builder()
-               .withQuery(queryText)
+        it('should build a TsGetReq correctly', function(done) {
+            var cmd = new TS.Get.Builder()
+               .withTable(table)
+               .withKey(key)
                .withCallback(function(){})
                .build();
-            var protobuf = queryCommand.constructPbRequest();
+            var protobuf = cmd.constructPbRequest();
+            assert.strictEqual(protobuf.getTable().toString('utf8'), table);
 
-            var tsi = protobuf.getQuery();
-            assert(tsi, 'expected Tsinterpolation');
-
-            var base = tsi.getBase().toString('utf8');
-            assert.strictEqual(base, queryText);
-
-            var i = tsi.getInterpolations();
-            assert(Array.isArray(i));
-            assert.strictEqual(i.length, 0);
+            var tsc = protobuf.getKey();
+            for (var i = 0; i < key.length; i++) {
+                assert.strictEqual(tsc[i].toString('utf8'), key[i]);
+            }
 
             done();
         });
         
-        it('should take a TsQueryResp and call the users callback with the response', function(done) {
+        it('should take a TsGetResp and call the users callback with the response', function(done) {
             var cb = function(err, response) {
                 assert(!err, err);
-                d.validateResponse(response, d.tsQueryResp);
+                d.validateResponse(response, d.tsGetResp);
                 done();
             };
-            var queryCommand = new TS.Query.Builder()
-               .withQuery(queryText)
+            var cmd = new TS.Get.Builder()
+               .withTable(table)
+               .withKey(key)
                .withCallback(cb)
                .build();
-            queryCommand.onSuccess(d.tsQueryResp);
+            cmd.onSuccess(d.tsGetResp);
         });
         
         it ('should take a RpbErrorResp and call the users callback with the error message', function(done) {
            var rpbErrorResp = new RpbErrorResp();
            rpbErrorResp.setErrmsg(new Buffer('this is an error'));
-           
            var cb = function(err, response) {
                 assert(err, !err);
                 assert.strictEqual(err, 'this is an error');
                 done();
             };
-           
-            var queryCommand = new TS.Query.Builder()
-               .withQuery(queryText)
+            var cmd = new TS.Get.Builder()
+               .withTable(table)
+               .withKey(key)
                .withCallback(cb)
                .build();
-       
-            queryCommand.onRiakError(rpbErrorResp);
+            cmd.onRiakError(rpbErrorResp);
         });
     });
 });
